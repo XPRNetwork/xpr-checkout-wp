a1b2d0bd479735366667fea50510caafd0c7902d
Remy Chauveau <remy@bequest.world>
2023-07-18 12:12:56 +0200

    Fix price formated to handle token precision

--- scripts/js/public/utils/price.ts
+++ scripts/js/public/utils/price.ts
@@ -1,4 +1,6 @@
 export function truncateToPrecision(number: number,precision: number) {
   const multiplier = Math.pow(10, precision);
+  console.log(number * multiplier,precision,'number * multiplier');
+  console.log(Math.trunc(number * multiplier),"truncated value");
   return Math.trunc(number * multiplier) / multiplier;
 }
