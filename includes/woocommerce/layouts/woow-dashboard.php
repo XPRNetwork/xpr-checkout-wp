b24489fd1b8c3e67729453c57ec861643df3522f
Remy Chauveau <remy@bequest.world>
2023-07-25 10:46:36 +0200

    Format infor for payment in woo dashboard

--- includes/woocommerce/layouts/woow-dashboard.php
+++ includes/woocommerce/layouts/woow-dashboard.php
@@ -13,16 +13,20 @@ class WoowDashboadLayout
 
     ob_start();
 ?>
-    <div>
-      <div>
-        <h4>Payments</h4>
-        <?php echo $this->renderPayments()  ?>
-      </div>
-      <div>
-        <h4>Payouts</h4>
+    <div class="wrap">
+      <div id="poststuff">
+        <div id="post-body" class="columns-2">
+          <div id="postbox-container-1">
+
+          </div>
+          <div id="postbox-container-2">
+            <?php echo $this->renderPayments()  ?>
+          </div>
+
+        </div>
+        <br class="clear">
       </div>
     </div>
-
   <?php
     return ob_get_clean();
   }
@@ -37,18 +41,68 @@ class WoowDashboadLayout
   ?>
     <table class="wp-list-table widefat fixed striped table-view-list posts">
       <thead>
-        <td>Amount</td>
-        <td>Buyer</td>
-        <td>Payment key</td>
+        <td>Order</td>
         <td>Status</td>
+        <td>Total</td>
       </thead>
       <tbody>
         <?php foreach ($payments['rows'] as $row) : ?>
           <tr>
+            <td>
+              <?php
+              $args = array(
+                'post_type'      => 'shop_order',
+                'post_status'    => 'any',
+                'meta_key'       => '_paymentKey', // Meta key for paymentKey
+                'meta_value'     => $row['paymentKey'],
+                'meta_compare'   => '=',
+                'posts_per_page' => 1,
+              );
+
+              $ordersQuery = wc_get_orders($args);
+              if ($ordersQuery && isset($ordersQuery[0])) {
+                $url = admin_url('post.php?post=' . $ordersQuery[0]->get_id()) . '&action=edit';
+                $orderLabel = "#" . $ordersQuery[0]->get_id() . " " . $row['buyer'];
+                echo '<a href="' . $url . '"> <b>' . $orderLabel . '</b> </a>';
+              }
+              ?>
+            </td>
+            <td>
+              <?php
+              //TODO: Rely on CSS class 
+              $row['status'];
+              switch ($row['status']) {
+
+                case -2:
+                  $bgColor = "#e1c6d4";
+                  $text = "Refunded";
+                  break;
+
+                case -1:
+                  $bgColor = "#ded5aa";
+                  $text = "Canceled";
+                  break;
+
+                case 0:
+                  $bgColor = "#c8d7e1";
+                  $text = "Await";
+                  break;
+
+                case 1:
+                  $bgColor = "#c6e1c6";
+                  $text = "Fulfilled";
+                  break;
+
+                case 2:
+                  $bgColor = "#c6e1c6";
+                  $text = "Payed out";
+                  break;
+              }
+              echo '<span class="button-primary" style="color:#50575e;background-color:' . $bgColor . ';" >' . $text . '</span>';
+              ?>
+            </td>
             <td><?php echo $row['amount'] ?></td>
-            <td><?php echo $row['buyer'] ?></td>
-            <td><?php echo $row['paymentKey'] ?></td>
-            <td><?php echo $row['status'] ?></td>
+
           </tr>
         <?php endforeach ?>
       </tbody>
