4ad581a3d55f686757c170a27686177f7594ac62
Remy Chauveau <remy@bequest.world>
2023-08-10 00:01:12 +0200

    Clean common types

--- assets/js/commons/type.d.ts
+++ assets/js/commons/type.d.ts
@@ -9,30 +9,3 @@ export interface TokenRate{
     price_usd:number
   }
 }
-
-export interface ProtonWCControllerOption {
-
-  mainwallet?:string;
-  testwallet?:string;
-  testnet?:boolean;
-  appName?:string;
-  appLogo?:string;
-  allowedTokens?:string;
-  wooCurrency?:string;
-  paymentKey:string;
-  order:{
-    total:number,
-    status:string
-  }
-  
-  }
-  
-  export interface ProtonCheckOutState {
-  
-  appState?:string;
-  isRunning:boolean,
-  session?:LinkSession
-  tx?:TransactResult,
-  order?:any
-  
-  }
