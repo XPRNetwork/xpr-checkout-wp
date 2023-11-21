e5e71f03722a20c072a4b849f9aa8b62571584b6
Remy Chauveau <remy@bequest.world>
2023-11-21 21:52:24 +0100

    Add support for external jQuery woocomerce checkout form

---
+++ assets/js/commons/jqEvent.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+  import {createEventDispatcher} from 'svelte';
+  const dispatch = createEventDispatcher();
+
+  if (window && window.jQuery) { 
+
+  
+  window.jQuery('input').on('blur', function () {
+
+    dispatch('formEvent',validateCheckoutForm())
+
+   })
+
+  function validateCheckoutForm():boolean {
+  
+    let invalidCount = 0
+    if (window && window.jQuery) {
+      window.jQuery('.validate-required input, .validate-required select').trigger('validate');
+      jQuery('.woocommerce-invalid').each(
+        function (index, element) {
+          if (element) invalidCount++
+        }
+      )
+    } else {
+      return true
+    }
+    return invalidCount == 0
+
+  }
+
+}
+</script>
