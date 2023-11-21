171bbc9c7c7f3d14123ed1840953c811ea178e69
Remy Chauveau <remy@bequest.world>
2023-11-21 22:41:53 +0100

    Remove the place order button when using only Wookey

--- assets/js/public/main.ts
+++ assets/js/public/main.ts
@@ -6,10 +6,15 @@ let app = new App({
 })
 if (window.jQuery) {
 
-  window.jQuery( document.body ).on('updated_checkout', () => {
+  window.jQuery(document.body).on('updated_checkout', () => {
+    const checkoutButton = document.querySelector('button[name="woocommerce_checkout_place_order"]');
+    if (checkoutButton) {
+      checkoutButton.style.display = "none"
+    }
+    console.log(checkoutButton,"?")
     const elm = document.getElementById('wookey-checkout')
     if (elm)elm.innerHTML = '';
-    console.log('the checkout mfer is called');
+    
     app = new App({
       target: document.getElementById('wookey-checkout'),
     
