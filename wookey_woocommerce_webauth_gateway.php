34e576386c0c6caa9aa9f686957c915496063a02
Remy Chauveau <remy@bequest.world>
2023-11-28 23:35:03 +0100

    Fix plugin crash when woocommerce is not activated/installed. Addwp admin notice. Update plugin semver

--- wookey_woocommerce_webauth_gateway.php
+++ wookey_woocommerce_webauth_gateway.php
@@ -4,7 +4,7 @@
  * Description: Allow user to pay securly with with multiple crypto currencies through Webauth with NO GAS FEE BABY !
  * Author: Rémy Chauveau AKA Rockerone
  * Author URI: hypersolid.io
- * Version: 1.0.7-beta
+ * Version: 1.0.8-beta
  * Text Domain: wookey
  * Domain Path: /i18n/languages/
  */
