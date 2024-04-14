4642c17d787a43f1d905034fc9913304944d8719
Remy Chauveau <rem.chauveau@gmail.com>
2024-04-14 21:48:14 +0200

    Fix method input radio handler to hide place order button on wookey payment method

--- assets/js/public/main.ts
+++ assets/js/public/main.ts
@@ -1,28 +1,40 @@
-import App from './App.svelte'
+import App from "./App.svelte";
 
 let app = new App({
-  target: document.getElementById('wookey-checkout'),
-
-})
+  target: document.getElementById("wookey-checkout"),
+});
 if (window.jQuery) {
-
-  window.jQuery(document.body).on('updated_checkout', () => {
-    const checkoutButton = document.querySelector('button[name="woocommerce_checkout_place_order"]');
-    if (checkoutButton) {
-      checkoutButton.style.display = "none"
-    }
-    console.log(checkoutButton,"?")
-    const elm = document.getElementById('wookey-checkout')
-    if (elm)elm.innerHTML = '';
-    
-    app = new App({
-      target: document.getElementById('wookey-checkout'),
-    
-    })
-  
-  })
-  
+  updateOrderButton()
+  window.jQuery(document.body).on("updated_checkout", () => {
+    let app = new App({
+      target: document.getElementById("wookey-checkout"),
+    });
+    updateOrderButton()
+  });
 }
 
+function updateOrderButton() {
+  const methodRadio = document.querySelectorAll(
+    ".wc_payment_method .input-radio"
+  );
+  const checkoutButton = document.querySelector(
+    'button[name="woocommerce_checkout_place_order"]'
+  );
+  Array.prototype.slice.call(methodRadio).map(r => {
+    if (checkoutButton) { 
 
-export default app
+      checkoutButton.style.display = r.value == "wookey" ? "none" : "block";
+    }
+    r.addEventListener("change", e => {
+      if (e.target) {
+        
+        if (checkoutButton) {
+          checkoutButton.style.display =
+            e.target.value == "wookey" ? "none" : "block";
+        }
+      }
+    });
+  });
+}
+
+export default app;
