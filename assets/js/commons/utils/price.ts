1bc00e145eb5e50a2c19778729eacfd26490ee3f
Remy Chauveau <rem.chauveau@gmail.com>
2024-04-25 01:36:47 +0200

    Adding a toPrecision function.

--- assets/js/commons/utils/price.ts
+++ assets/js/commons/utils/price.ts
@@ -2,3 +2,29 @@ export function truncateToPrecision(number: number | string, precision: number):
   const numberToTruncate = typeof number == 'string'? parseFloat(number) : number
   return numberToTruncate.toFixed(precision);
 }
+
+export function toPrecision(value: number, precision: number,mode:'ceil'|'floor'|'round'|'none' = 'ceil',forceDecimal:boolean = true) {
+  
+  const multiplier = Math.pow(10, precision);
+  let powValue = value * multiplier;
+  switch (mode) {
+    
+    case 'ceil':
+      powValue = Math.ceil(powValue);
+      break;
+    case 'floor':
+      powValue = Math.floor(powValue);
+      break;
+    case 'round':
+      powValue = Math.round(powValue);
+      break;
+    case 'none':
+      powValue = Math.ceil(powValue);
+      break;
+  }
+
+  const mutatedValue = powValue / multiplier
+  if (forceDecimal) return mutatedValue.toFixed(precision)
+  return mutatedValue.toString() ;
+
+}
