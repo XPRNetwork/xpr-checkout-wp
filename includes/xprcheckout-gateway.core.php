8ee5d1c00d4d2e4216457d5a9a873274fb8c7e58
Remy Chauveau <rem.chauveau@gmail.com>
2024-11-08 11:48:11 +0100

    Remove unused script and classes from xpr_checkout_gateway.core.php

--- includes/xprcheckout-gateway.core.php
+++ includes/xprcheckout-gateway.core.php
@@ -18,28 +18,26 @@ class ProtonWcGateway
 
     require_once XPRCHECKOUT_ROOT_DIR . 'includes/controllers/Cart.php';
     require_once XPRCHECKOUT_ROOT_DIR . 'includes/controllers/Config.php';
-    require_once XPRCHECKOUT_ROOT_DIR . 'includes/controllers/DashBoard.php';
+    
     require_once XPRCHECKOUT_ROOT_DIR . 'includes/controllers/Gateway.php';
     require_once XPRCHECKOUT_ROOT_DIR . 'includes/controllers/i18n.php';
     require_once XPRCHECKOUT_ROOT_DIR . 'includes/controllers/Notices.php';
     require_once XPRCHECKOUT_ROOT_DIR . 'includes/controllers/Orders.php';
-    
     require_once XPRCHECKOUT_ROOT_DIR . 'includes/controllers/Refund.php';
     require_once XPRCHECKOUT_ROOT_DIR . 'includes/controllers/RegStore.php';
 
     require_once XPRCHECKOUT_ROOT_DIR . 'includes/rpc/ProtonRPC.php';
     require_once XPRCHECKOUT_ROOT_DIR . 'includes/rpc/PriceRateRPC.php';
     require_once XPRCHECKOUT_ROOT_DIR . 'includes/rpc/TokensPricesRPC.php';
-
-    require_once XPRCHECKOUT_ROOT_DIR . 'includes/api/payment-check.php';
-    require_once XPRCHECKOUT_ROOT_DIR . 'includes/api/verify-transaction.php';
-    require_once XPRCHECKOUT_ROOT_DIR . 'includes/api/price-rates.php';
-    require_once XPRCHECKOUT_ROOT_DIR . 'includes/api/tokens-prices.php';
-    require_once XPRCHECKOUT_ROOT_DIR . 'includes/api/cart.php';
-    require_once XPRCHECKOUT_ROOT_DIR . 'includes/api/update-order.php';
-    require_once XPRCHECKOUT_ROOT_DIR . 'includes/api/admin-payments-with-orders.php';
+    require_once XPRCHECKOUT_ROOT_DIR . 'includes/rpc/AlcorPricesRPC.php';
+    
+    require_once XPRCHECKOUT_ROOT_DIR . 'includes/api/order-payment.php';
+    require_once XPRCHECKOUT_ROOT_DIR . 'includes/api/admin-refund.php';
 
     require_once XPRCHECKOUT_ROOT_DIR . 'includes/utils/order-resolver.php';
+    require_once XPRCHECKOUT_ROOT_DIR . 'includes/utils/symbol.php';
+    require_once XPRCHECKOUT_ROOT_DIR . 'includes/utils/to-precision.php';
+    require_once XPRCHECKOUT_ROOT_DIR . 'includes/utils/order-by-payment-key.php';
     
     
   }
@@ -48,13 +46,13 @@ class ProtonWcGateway
   {
 
     $gateway    = new \xprcheckout\gateway\GatewayWrapper();
-    $dashboard  = new \xprcheckout\admin\Dashboard();
+    
     $refund     = new \xprcheckout\admin\Refund();
     $regStore     = new \xprcheckout\admin\RegStore();
     
     $orders     = new \xprcheckout\admin\Orders();
     $notices    = new \xprcheckout\admin\Notices();
-    $cart       = new \xprcheckout\cart\Cart();
+    
     
   }
 }
