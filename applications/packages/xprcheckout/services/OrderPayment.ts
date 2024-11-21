cf3c44e0eada3bf3c203f9672d4b8b320b61cccb
Remy Chauveau <rem.chauveau@gmail.com>
2024-11-21 13:58:57 +0100

    Add support for txId storage with order meta

--- applications/packages/xprcheckout/services/OrderPayment.ts
+++ applications/packages/xprcheckout/services/OrderPayment.ts
@@ -1,9 +1,10 @@
 import type { WPResponse,OrderPayment } from "../interfaces";
 
-export async function getOrderPayment(baseDomain:string,paymentKey: string) {
+export async function getOrderPayment(baseDomain:string,paymentKey: string,txId?:string) {
   
   const bodyData = {
-    paymentKey:paymentKey
+    paymentKey: paymentKey,
+    txId
   };
   const fetchResult = await fetch(`${baseDomain}/wp-json/xprcheckout/v2/order-payment`,{
     method: 'post',
